3897175544f6e06c6d95d4588feb8abb869fe005
Brandon Culp <bculp513@gmail.com>
2023-09-22 15:49:29 -0600

    Investments in Checking mvp

---
+++ app/components/InvestmentChecking/index.tsx
@@ -0,0 +1,38 @@
+import { Title } from "@mantine/core";
+import styles from "../Shared/Layout.module.css";
+import { NumberInput } from "../Shared/NumberInput";
+
+export const InvestmentChecking = ({
+  rothIRA,
+  individualInvestments,
+  mutualFunds,
+}: {
+  rothIRA: number;
+  individualInvestments: number;
+  mutualFunds: number;
+}) => (
+  <div>
+    <Title className={styles.header} order={3}>
+      Total In Checking
+    </Title>
+    <div className={styles.container}>
+      <div className={styles.section}>
+        <NumberInput
+          className={styles.input}
+          label="Total in checking that should go to IRA"
+          value={rothIRA}
+        />
+        <NumberInput
+          className={styles.input}
+          label="Total in checking that should go to Individual Investments"
+          value={individualInvestments}
+        />
+        <NumberInput
+          className={styles.input}
+          label="Total in checking that should go to ETFs/Mutual Funds"
+          value={mutualFunds}
+        />
+      </div>
+    </div>
+  </div>
+);
