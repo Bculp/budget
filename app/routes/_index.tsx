70e7461b5f14966bee3b15c022d3fe756c89f4ca
Brandon Culp <bculp513@gmail.com>
2023-09-22 15:49:41 -0600

    Render all the mvps

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
 import type { V2_MetaFunction } from "@remix-run/node";
+import { Expenses } from "~/components/Expenses";
 import { Income } from "~/components/Income/Income";
+import { Investments } from "~/components/Investments";
+import { Totals } from "~/components/Totals";
+import { Savings } from "~/components/Savings";
+import { InvestmentChecking } from "~/components/InvestmentChecking";
+
+export interface ExpenseTotal {
+  fixed: number;
+  variable: number;
+  fun: number;
+}
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -9,9 +21,51 @@ export const meta: V2_MetaFunction = () => {
 };
 
 export default function Index() {
+  const [incomeTotal, updateIncomeTotal] = useState(0);
+  const [expenseTotal, updateExpenseTotal] = useState({
+    fixed: 0,
+    variable: 0,
+    fun: 0,
+  });
+  const expenses =
+    expenseTotal.fixed + expenseTotal.variable + expenseTotal.fun;
+  const [investmentTotal, updateInvestmentTotal] = useState(0);
+  const amtAvailableToInvest = incomeTotal - expenses;
+  const savings = amtAvailableToInvest - investmentTotal;
+
+  const [rothIRA, updateRothIRA] = useState(0);
+  const [individualInvestments, updateIndividualInvestments] = useState(0);
+  const [mutualFunds, updateMutualFunds] = useState(0);
+
   return (
     <div>
-      <Income />
+      <Income updateIncomeTotal={updateIncomeTotal} />
+      <Expenses
+        expenseTotal={expenseTotal}
+        updateExpenseTotal={updateExpenseTotal}
+      />
+      <Investments
+        rothIRA={rothIRA}
+        updateRothIRA={updateRothIRA}
+        individualInvestments={individualInvestments}
+        updateIndividualInvestments={updateIndividualInvestments}
+        updateMutualFunds={updateMutualFunds}
+        mutualFunds={mutualFunds}
+        updateInvestmentTotal={updateInvestmentTotal}
+      />
+      <Totals
+        income={incomeTotal}
+        expenses={expenses}
+        investments={investmentTotal}
+        amtAvailableToInvest={amtAvailableToInvest}
+        savings={savings}
+      />
+      <Savings savings={savings} />
+      <InvestmentChecking
+        rothIRA={rothIRA}
+        individualInvestments={individualInvestments}
+        mutualFunds={mutualFunds}
+      />
     </div>
   );
 }
