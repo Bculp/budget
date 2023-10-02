d4060d85d40c937e896755b2a195b7f8ee639afd
Brandon Culp <bculp513@gmail.com>
2023-10-02 11:52:59 -0600

    Update and Delete work as expected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { Dog } = require('./db/models/totals');
 const { Month } = require('./db/models/month');
 const app = express();
 const port = 4000;
@@ -13,13 +12,10 @@ const formatId = (year, month) => {
 
 app.use(express.json());
 
+// GET ALL //
+
 app.get('/', (req, res) => {
   res.json('Hello World!');
-});
-
-app.get('/dogs', async (req, res) => {
-  const allDogs = await Dog.find();
-  return res.status(200).json(allDogs);
 });
 
 app.get('/month', async (req, res) => {
@@ -27,11 +23,7 @@ app.get('/month', async (req, res) => {
   return res.status(200).json(allMonths);
 });
 
-app.get('/dogs/:id', async (req, res) => {
-  const { id } = req.params;
-  const dog = await Dog.findById(id);
-  return res.status(200).json(dog);
-});
+// GET BY ID //
 
 app.get('/month/:id', async (req, res) => {
   const { id } = req.params;
@@ -39,11 +31,7 @@ app.get('/month/:id', async (req, res) => {
   return res.status(200).json(month);
 });
 
-app.post('/dogs', async (req, res) => {
-  const newDog = new Dog({ ...req.body });
-  const insertedDog = await newDog.save();
-  return res.status(201).json(insertedDog);
-});
+// POST //
 
 app.post('/month', async (req, res) => {
   const newMonth = new Month({
@@ -54,17 +42,30 @@ app.post('/month', async (req, res) => {
   return res.status(201).json(insertedMonth);
 });
 
-app.put('/dogs/:id', async (req, res) => {
+// PUT //
+
+app.put('/month/:id', async (req, res) => {
   const { id } = req.params;
-  await Dog.updateOne({ id }, req.body);
-  const updatedDog = await Dog.findById(id);
-  return res.status(200).json(updatedDog);
+  let month = await Month.find({ id });
+  const updatedMonth = await Month.findOneAndUpdate(
+    { id },
+    {
+      $set: {
+        Income: { ...month.Income, ...req.body.Income },
+        Expenses: { ...month.Expenses, ...req.body.Expenses },
+      },
+    },
+    { new: true }
+  );
+  return res.status(200).json(updatedMonth);
 });
 
-app.delete('/dogs/:id', async (req, res) => {
+// DELETE //
+
+app.delete('/month/:id', async (req, res) => {
   const { id } = req.params;
-  const deletedDog = await Dog.findByIdAndDelete(id);
-  return res.status(200).json(deletedDog);
+  const deletedMonth = await Month.findOneAndDelete({ id });
+  return res.status(200).json(deletedMonth);
 });
 
 // Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
