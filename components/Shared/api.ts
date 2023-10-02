7e42f47a4e798eca92a94a8b716406be5fc6c3c1
Brandon Culp <bculp513@gmail.com>
2023-10-02 15:55:26 -0600

    Added api fns to hit express server

---
+++ components/Shared/api.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const url = 'http://localhost:4000';
+
+export const getAllMonths = async () => {
+  try {
+    const response = await axios.get(`${url}/month`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getMonth = async (id: string) => {
+  try {
+    const response = await axios.get(`${url}/month/${id}`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createMonth = async (body: any) => {
+  try {
+    const response = await axios.post(`${url}/month`, body);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateMonth = async (id: string) => {
+  try {
+    const response = await axios.put(`${url}/month/${id}`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteMonth = async (id: string) => {
+  try {
+    const response = await axios.delete(`${url}/month/${id}`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
